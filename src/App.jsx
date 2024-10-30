b3582dd6fd566eaa14d6c220f4c5cf51de2eef41
Nguyen Duong <nguyenduong939705@gmail.com>
2024-10-30 13:08:07 +0700

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,39 +9,69 @@ import ListPage from "./routes/listPage/listPage";
 import Layout from "./routes/layout/layout";
 import SinglePage from "./routes/singlePage/singlePage";
 import ProfilePage from "./routes/profilePage/profilePage";
+import Login from "./routes/login/login";
+import Register from "./routes/register/register";
+
 
 function App() {
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: 
-        <Layout/>,
-        children:[
-          {
-            path:"/",
-            element:<HomePage/>
-          },
-          {
-            path:"/list",
-            element:<ListPage/>
-          },
-          {
-            path:"/:id",
-            element:<SinglePage/>
-          },
-          {
-            path:"/profile",
-            element:<ProfilePage/>
-          },
-        ]      
+      element:
+        <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />
+        },
+        {
+          path: "/list",
+          element: <ListPage />
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />
+        },
+        {
+          path: "/login",
+          element: <Login />,
+        },
+        {
+          path: "/register",
+          element: <Register />,
+        },
+        {
+          path: "/profile",
+          element: <ProfilePage />
+        },
+      ],
     },
-    
+
+    // {
+    //   path: "/",
+    //   element: <RequireAuth />,
+    //   children: [
+    //     {
+    //       path: "/profile",
+    //       element: <ProfilePage />,
+    //       loader: profilePageLoader
+    //     },
+    //     {
+    //       path: "/profile/update",
+    //       element: <ProfileUpdatePage />,
+    //     },
+    //     {
+    //       path: "/add",
+    //       element: <NewPostPage />,
+    //     },
+    //   ],
+    // },
   ]);
 
   return (
-    
-    <RouterProvider router ={router}/>
+
+    <RouterProvider router={router} />
   )
 }
 
